268662ef27f8261262508f2acdce9f453e09e8d3
SDever <72769325+TeegardenMath@users.noreply.github.com>
2024-04-26 13:25:09 -0700

    first test

--- test/reverse-string-spec.js
+++ test/reverse-string-spec.js
@@ -1,1 +1,10 @@
-// Your code here
+const chai = require("chai");
+const expect = chai.expect;
+const reverseString = require("../problems/reverse-string")
+
+describe("reverseString(word)", () => {
+	it("should reverse a simple string", () => {
+		const word = "fun";
+		expect(reverseString(word)).to.equal("nuf");
+	});
+});
